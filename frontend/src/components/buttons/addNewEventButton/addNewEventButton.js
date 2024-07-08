75faa20dda964b25ab8a693ee0e7dbd9ac90e90a
quintero git <hello.quinteroo@gmail.com>
2024-07-08 20:37:09 +0200

    fear: formNewEvent.js

--- frontend/src/components/buttons/addNewEventButton/addNewEventButton.js
+++ frontend/src/components/buttons/addNewEventButton/addNewEventButton.js
@@ -1,5 +1,5 @@
 import "./addNewEventButton.css"
-import { home } from "../../../pages/home/home.js"
+import { formNewEvent } from "../../forms/formNewEvent/formNewEvent.js"
 
 
 
@@ -24,84 +24,9 @@ export const addNewEventButton = () => {
     }
   })
 
-
-
-  // ESTRUCTURA FORMULARIO
-
-  formEvent.innerHTML =
-    `
-    <h3>LET'S GO</h3>
-    <input type="text" id="inputEventTitle" name="title" placeholder="Título" required>
-    
-    <input type="text" id="inputEventLocation" name="location" placeholder="Ubicación" required>
-
-    <input type="date" id="inputEventDate" name="date" required>
-    
-    <select id="inputEventStyle" name="style" required>
-      <option value="">Selecciona un estilo</option>
-      <option value="rock">Rock</option>
-      <option value="heavy">Heavy</option>
-      <option value="rap">Rap</option>
-      <option value="pop">Pop</option>
-      <option value="indie">Indie</option>
-      <option value="reggae">Reggae</option>
-      <option value="electro">Electro</option>
-      <option value="flamenco">Flamenco</option>
-      <option value="techno">Techno</option>
-    </select><br>
-    
-    <textarea id="inputDescription" name="description" placeholder="Descripción" required></textarea>
-    
-    <input type="file" id="inputEventPoster" name="image" accept="image/*" required>
-    
-    <button type="submit" class="primary-button">CREAR EVENTO</button>
-  `
-
-  // EVENTO FORMULARIO BBDD
-  const inputEventTitle = document.querySelector("#inputEventTitle")
-  const inputEventLocation = document.querySelector("#inputEventLocation")
-  const inputEventDate = document.querySelector("#inputEventDate")
-  const inputEventStyle = document.querySelector("#inputEventStyle")
-  const inputDescription = document.querySelector("#inputDescription")
-  const inputEventPoster = document.querySelector("#inputEventPoster")
-
+  // FORMULARIO
+  formNewEvent(formEvent)
 
   main.append(addEventButton)
   main.append(formEvent)
-
-
-  formEvent.addEventListener("submit", () => {
-    submit(inputEventTitle.value, inputEventLocation.value, inputEventDate.value, inputEventStyle.value, inputDescription.value, inputEventPoster.value)
-  });
-
-  console.log(inputEventTitle);
-  console.log(inputEventDate);
-  console.log(inputEventLocation);
-
-
-}
-
-
-const submit = async (title, location, date, style, description, poster) => {
-
-  const opciones = {
-    method: "POST",
-    body: JSON.stringify({
-      title,
-      location,
-      date,
-      style,
-      description,
-      poster
-    }),
-    headers: {
-      "content-type": "application/json"
-    }
-  }
-
-  const res = await fetch(`${URL}user/event`, opciones)
-  const resFinal = await res.json()
-  console.log(resFinal);
-
-  home()
 }
--- frontend/src/components/forms/formNewEvent/formNewEvent.js
+++ frontend/src/components/forms/formNewEvent/formNewEvent.js
@@ -1,8 +1,84 @@
 import "./formNewEvent.css"
+import { home } from "../../../pages/home/home.js"
 
-export const formNewEvent = () => {
+export const formNewEvent = (elementoPadre) => {
 
-  const main = document.querySelector("main")
+  const inputTitle = document.createElement("input")
+  const inputLocation = document.createElement("input")
+  const inputStyle = document.createElement("select")
+  const inputDate = document.createElement("input")
+  const inputDescription = document.createElement("textarea")
+  const inputPoster = document.createElement("input")
+  const buttonCreateEvent = document.createElement("button")
+  buttonCreateEvent.classList.add("primary-button", "hover")
+
+  inputTitle.type = "text"
+  inputTitle.placeholder = "Nombre del evento"
+
+  inputLocation.type = "text"
+  inputLocation.placeholder = "Localización del evento"
+
+  inputStyle.innerHTML =
+    `
+    <option value="">Selecciona un estilo</option>
+    <option value="rock">Rock</option>
+    <option value="heavy">Heavy</option>
+    <option value="rap">Rap</option>
+    <option value="pop">Pop</option>
+    <option value="indie">Indie</option>
+    <option value="reggae">Reggae</option>
+    <option value="electro">Electro</option>
+    <option value="flamenco">Flamenco</option>
+    <option value="techno">Techno</option>
+  `
+  inputDate.type = "date"
+
+  inputDescription.placeholder = "Describe el evento"
+
+  inputPoster.type = "file"
+  inputPoster.accept = "image/*"
+
+  buttonCreateEvent.textContent = "Crear Evento"
+
+  elementoPadre.append(inputTitle)
+  elementoPadre.append(inputLocation)
+  elementoPadre.append(inputDate)
+  elementoPadre.append(inputStyle)
+  elementoPadre.append(inputDescription)
+  elementoPadre.append(inputPoster)
+  elementoPadre.append(buttonCreateEvent)
 
 
+
+
+
+  // elementoPadre.addEventListener("submit", () => {
+  //   submit(inputEventTitle.value, inputEventLocation.value, inputEventDate.value, inputEventStyle.value, inputDescription.value, inputEventPoster.value)
+  // });
 }
+
+
+
+// const submit = async (title, location, date, style, description, poster) => {
+
+//   const opciones = {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title,
+//       location,
+//       date,
+//       style,
+//       description,
+//       poster
+//     }),
+//     headers: {
+//       "content-type": "application/json"
+//     }
+//   }
+
+//   const res = await fetch(`${URL}user/event`, opciones)
+//   const resFinal = await res.json()
+//   console.log(resFinal);
+
+//   home()
+// }
