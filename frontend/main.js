490ca42ab314ed36a1cf848a6dcaa63dd0feef63
quintero git <hello.quinteroo@gmail.com>
2024-08-06 21:07:32 +0200

    fear: actualizar navegador

--- frontend/main.js
+++ frontend/main.js
@@ -4,11 +4,16 @@ import { logo } from "./src/components/logo/logo.js"
 import { storeFront } from "./src/components/storeFront/storeFront.js"
 import { footer } from "./src/components/footer/footer.js"
 import { intro } from "./src/components/intro/intro.js"
+import { home } from './src/pages/home/home.js';
 
 
 
 
 export const landingPage = () => {
+  const token = localStorage.getItem("loginToken")
+  if (token) {
+    home()
+  }
   const divApp = document.querySelector("#app")
   divApp.innerHTML = "";
 
