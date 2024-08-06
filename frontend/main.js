349b67e43b6069d13210aa902d7967de48a2876c
quintero git <hello.quinteroo@gmail.com>
2024-08-06 21:24:37 +0200

    fear: manear reinicio de la aplicación

--- frontend/main.js
+++ frontend/main.js
@@ -10,10 +10,6 @@ import { home } from './src/pages/home/home.js';
 
 
 export const landingPage = () => {
-  const token = localStorage.getItem("loginToken")
-  if (token) {
-    home()
-  }
   const divApp = document.querySelector("#app")
   divApp.innerHTML = "";
 
@@ -28,4 +24,16 @@ export const landingPage = () => {
 
 }
 
-document.addEventListener("DOMContentLoaded", landingPage);
+// Función principal para manejar la carga inicial
+function handleInitialLoad() {
+  const token = localStorage.getItem('loginToken');
+  if (token) {
+    // Token existe, redirige a la página de inicio
+    home();
+  } else {
+    // Token no existe, muestra la página de inicio de sesión
+    landingPage();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", handleInitialLoad);
